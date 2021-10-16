b4836134d68812c8d18e16994bc5c2f878481f03
Ahsen Khaliq <ahsen.khaliq@gmail.com>
2021-10-16 06:33:43 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ import gradio as gr
 import torch
 from PIL import Image
 import numpy as np
-
+os.mkdir("data")
+os.mkdir("dataout")
 # Images
 torch.hub.download_url_to_file('https://cdn.pixabay.com/photo/2018/08/12/16/59/ara-3601194_1280.jpg', 'parrot.jpg')
 torch.hub.download_url_to_file('https://cdn.pixabay.com/photo/2016/10/21/14/46/fox-1758183_1280.jpg', 'fox.jpg')
@@ -25,8 +26,8 @@ def infer(img):
   im.save("./data/data_mask.png")
   os.system('python predict.py model.path=./big-lama indir=./data outdir=./dataout device=cpu')
   return "./dataout/data_mask.png"
-inputs = gr.inputs.Image(type='file', label="Original Image")
-outputs = gr.outputs.Image(type="numpy",label="output")
+inputs = gr.inputs.Image(type='pil', label="Original Image")
+outputs = gr.outputs.Image(type="file",label="output")
 title = "U^2-Net"
 description = "demo for U^2-Net. To use it, simply upload your image, or click one of the examples to load them. Read more at the links below."
 article = "<p style='text-align: center'><a href='https://arxiv.org/abs/2005.09007'>U^2-Net: Going Deeper with Nested U-Structure for Salient Object Detection</a> | <a href='https://github.com/xuebinqin/U-2-Net'>Github Repo</a></p>"
